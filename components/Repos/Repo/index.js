cdbf1732d2598218bac1f59588994b1f3e68056c
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-01-17 12:33:58 +0100

    Add slide in animations for repos

--- components/Repos/Repo/index.js
+++ components/Repos/Repo/index.js
@@ -1,11 +1,22 @@
+"use client";
+
 import { formatDate } from "@/utils/formatDate";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import css from "../index.module.css";
 
 export default function Repo({ repo }) {
+    const { ref, inView } = useInView();
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        if (inView && !visible) setVisible(true);
+    }, [inView]);
+
     return (
-        <div className={css.repo}>
+        <div className={`${css.repo}${visible ? ` ${css.visible}` : ""}`}>
             <a href={repo.html_url} className={css["repo-flag"]}></a>
-            <a href={repo.html_url} className={css["repo-content"]}>
+            <a href={repo.html_url} ref={ref} className={css["repo-content"]}>
                 {repo.created_at && <div className={css["repo-date"]}>{formatDate(repo.created_at)}</div>}
                 <div className={css["repo-name"]}>{repo.name}</div>
                 {repo.language && <div className={css["repo-language"]}> {repo.language}</div>}
